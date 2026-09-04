677d60cfb13558d1f495ee8e2c2a042c8de65b69
EnlistedGhost <enlisted.ghost@gmail.com>
2026-09-04 10:14:35 -0700

    Update version.go

--- version/version.go
+++ version/version.go
@@ -1,9 +1,3 @@
 package version
-// 0.33.3-xc11
-//
-// Github Will auto-bump 0.33.2 to correct version
-// If you are seeing matched version to the one
-// above then it's already been done and you may
-// ignore this comment.
-//
+
 var Version string = "0.33.3-xc11"
